1e069a8c001be409687a80eaa426d65711bbf461
Anil-Can <anilcan6840@gmail.com>
2022-11-30 20:49:52 +0300

    console eklendi

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors')
+const cors = require('cors');
+const chalk = require("chalk");
 
 const { getDistricts,getFeatures,getSpatialTables,createRecords } = require('./db/query.js');
 
@@ -16,6 +17,9 @@ app.get("/getFeatures",getFeatures)
 app.get("/getSpatialTables",getSpatialTables)
 app.post("/createRecords",createRecords)
 app.listen(process.env.PORT, () => {
-    
+    console.log(chalk.whiteBright("SERVER IS RUNNING PORT: ") + chalk.magenta(process.env.PORT))
+    console.log(chalk.whiteBright("HOST: ") + chalk.magenta(process.env.PGHOST))
+    console.log(chalk.whiteBright("USER: ") + chalk.magenta(process.env.PGUSER))
+    console.log(chalk.whiteBright("DATABASE: ") + chalk.magenta(process.env.PGDATABASE))
 });
 
